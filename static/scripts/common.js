0835d648a6b9c137919da4de8547e46198ecf4fd
Pete Warden <pete@petewarden.com>
2010-07-19 17:31:00 -0600

    Fixed bug with edit popups

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -415,3 +415,25 @@ function updateOpenHeatMapMessage(event)
     
     return true;
 }
+
+// From http://www.zipcodeworld.com/samples/distance.js.html
+function distanceFromLatLon(lat1, lon1, lat2, lon2, unit) 
+{
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var radlon1 = Math.PI * lon1/180
+    var radlon2 = Math.PI * lon2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    
+    if (unit=="K") { dist = dist * 1.609344 }
+    if (unit=="N") { dist = dist * 0.8684 }
+    
+    return dist
+}
+
