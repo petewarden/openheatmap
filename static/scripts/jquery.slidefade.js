e6032b0ccad1d59cdf8d3ce83c333c364792cdd1
Pete Warden <pete@petewarden.com>
2010-07-26 17:51:59 -0600

    Created slidefade plugin. Revamped front page to include more relevant links. Used slider for example csv files

---
+++ static/scripts/jquery.slidefade.js
@@ -0,0 +1,99 @@
+/**
+* author Pete Warden
+*/
+
+(function ($) {
+    $.fn.slidefade = function(elements, settings) {
+
+        this.__constructor = function(elements, settings) {
+
+            var defaults = {
+                leftX: 0,
+                midX: 260,
+                rightX: 520,
+                topY: 10,
+                fadeTime: 500,
+                holdTime: 3000
+            };
+     
+            if (settings) 
+                settings = $.extend(defaults, settings);
+            else
+                settings = defaults;
+
+            this.settings = settings;
+
+            this.animationFunctions = [];
+            
+            var self = this;
+            this.each(function() {
+            
+                $(this).empty();
+
+                for (var index in elements) {
+                    var element = elements[index];
+                                        
+                    var isLast = (index==(elements.length-1));
+                    var nextIndex;
+                    if (isLast) {
+                        nextIndex = 0;
+                    } else {
+                        nextIndex = ((Number)(index)+1);
+                    }
+
+                    self.animationFunctions[index] = function() {
+                        var myElement = element;
+                        var mySelf = self;
+                        var myNextIndex = nextIndex;
+
+                        return function() {
+
+                            myElement
+                            .stop()
+                            .css({display:'block'})
+                            .animate({left:mySelf.settings.midX+'px', opacity:1.0 }, mySelf.settings.fadeTime )
+                            .animate({opacity:1.0}, mySelf.settings.holdTime)
+                            .animate({left:mySelf.settings.leftX+'px', opacity:0.0 }, mySelf.settings.fadeTime, function() {
+                                mySelf.animationFunctions[myNextIndex]();
+                            })
+                            .animate({left:mySelf.settings.rightX+'px', opacity:0.0}, 0, function() { myElement.css({display:'none'}); });
+                        };
+                    }();
+
+                }
+                
+                var isFirst = true;
+                for (index in elements) {
+                    var element = elements[index];
+                    
+                    if (isFirst) {
+                        element.css({
+                            opacity: 1.0,
+                            position: 'absolute',
+                            left: self.settings.midX+'px',
+                            top: self.settings.topY+'px'
+                        });
+                    } else {
+                        element.css({
+                            opacity: 0.0,
+                            position: 'absolute',
+                            left: self.settings.rightX+'px',
+                            top: self.settings.topY+'px'
+                        });
+                    }
+                                        
+                    $(this).append(element);
+                    
+                    isFirst = false;
+                }
+                
+                self.animationFunctions[0]();
+            });
+            
+        };
+
+        this.__constructor(elements, settings);
+        
+        return this;
+    };
+}(jQuery));
